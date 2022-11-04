fefc1116d94d862201bb4cffc413ce940e8c3d35
prostiate <prostiate@gmail.com>
2022-11-04 17:09:50 +0700

    update test

--- tests/pranala_tests.py
+++ tests/pranala_tests.py
@@ -5,42 +5,63 @@ from efishery_test_case import Pranala
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        self.pranala = Pranala(2)
+        self.pranala = Pranala()
 
-    def test_zero(self):
-        """Test 0 multiplied by 2"""
+    def test_add_record(self):
+        """Test add_records"""
+        result = self.pranala.add_records('gurame', 'jawa tengah', 'cilacap', '50', '40000')
+        self.assertEqual(result, result)
 
-        # 0 multiplied by 2 return 0
-        result = self.pranala.multiply(0)
-        self.assertEqual(result, 0)
+    def test_delete_record(self):
+        """Test delete_records"""
+        result = self.pranala.delete_records(['uuid=79877fd3-1aa7-4b19-ad51-8246a3338b1f'])
+        self.assertEqual(result, result)
 
-    def test_natural_number(self):
-        """Test natural number 5 multiplied by 2"""
+    def test_get_aggregation_price(self):
+        """Test get_aggregation_price"""
+        result = self.pranala.get_aggregation_price('komoditas')
+        self.assertEqual(result, result)
 
-        # 5 multiplied by 2 return 10
-        result = self.pranala.multiply(5)
-        self.assertEqual(result, 10)
+    def test_get_all_by_commodity(self):
+        """Test get_all_by_commodity"""
+        result = self.pranala.get_all_by_commodity('gurame')
+        self.assertEqual(result, result)
 
-    def test_integer_number(self):
-        """Test integer number -7 multiplied by 2"""
+    def test_get_all_by_range(self):
+        """Test get_all_by_range"""
+        result = self.pranala.get_all_by_range('2,50000', '30,170', '2020-11-02,2022-11-02')
+        self.assertEqual(result, result)
 
-        # -7 multiplied by 2 return -14
-        result = self.pranala.multiply(-7)
-        self.assertEqual(result, -14)
+    def test_get_by_area(self):
+        """Test get_by_area"""
+        result = self.pranala.get_by_area('jawa tengah', 'cilacap')
+        self.assertEqual(result, result)
 
-    def test_rational_number(self):
-        """Test rational number 6/17 multiplied by 2"""
+    def test_get_by_id(self):
+        """Test get_by_id"""
+        result = self.pranala.get_by_id('2c75f19f-ac7a-4c28-9459-8fbb64c8cb00')
+        self.assertEqual(result, result)
 
-        # 6/17 multiplied by 2 return (6/17) * 2
-        result = self.pranala.multiply(6/17)
-        self.assertEqual(result, (6/17) * 2)
+    def test_get_by_range_price(self):
+        """Test get_by_range_price"""
+        result = self.pranala.get_by_range_price(5, 50000)
+        self.assertEqual(result, result)
 
-    def test_real_number(self):
-        """Test real number PI multiplied by 2"""
+    def test_get_max_price(self):
+        """Test get_max_price"""
+        result = self.pranala.get_max_price()
+        self.assertEqual(result, result)
 
-        # PI multiplied by 2 return 2PI
-        result = self.pranala.multiply(math.pi)
-        self.assertEqual(result, math.pi * 2)
+    def test_get_most_records(self):
+        """Test get_most_records"""
+        result = self.pranala.get_most_records()
+        self.assertEqual(result, result)
+
+    def test_update_records(self):
+        """Test update_records"""
+        result = self.pranala.update_records(['uuid=79877fd3-1aa7-4b19-ad51-8246a3338b1f'], ['size=170'])
+        self.assertEqual(result, result)
+
 
 
 if __name__ == '__main__':
